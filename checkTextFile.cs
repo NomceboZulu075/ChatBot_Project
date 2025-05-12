90232190410d107e2aa505e8699ea3a5199e5d06
NomceboZulu075 <ST10452658@rcconnect.edu.za>
2025-05-12 00:50:43 +0000

    Creating a string method that will return the path of the Memory Recall text file.

--- checkTextFile.cs
+++ checkTextFile.cs
@@ -1,11 +1,26 @@
-﻿namespace ChatBot_Project
+﻿using System.IO;
+using System;
+
+namespace ChatBot_Project
 {
     //Changing the internal keyword to public keyword
     public class checkTextFile
     {
-        public checkTextFile()
+        //Creating a string method that will return a path
+        //AppDomain as a global gives an error, therefore, I will do a return method that will return the path
+        private string returnPath()
         {
+            //Using an AppDomain to get me the full path
+            string fullPath = AppDomain.CurrentDomain.BaseDirectory;
 
-        }//end of constructor
+            //Replacing the bin\debug\ so it can get the path of the text file
+            string newPath = fullPath.Replace("bin\\Debug\\", "");
+
+            //Combining the path of the text file with the updated path
+            string path = Path.Combine(newPath, "MemoryRecallFile.txt");
+
+            return path;
+        }
+        
     }//end of class
 }//end of namespace
