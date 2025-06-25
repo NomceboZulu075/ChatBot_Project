59ed3e619cdbacc6b3cdf60fd57c0444166c8b96
NomceboZulu075 <ST10452658@rcconnect.edu.za>
2025-06-25 13:26:31 +0000

    Fixing and restructuring my Program (Main) class, adding try and catch, remoiving unecessary comments

--- Program.cs
+++ Program.cs
@@ -11,51 +11,56 @@ namespace ChatBot_Project
     {
         static void Main(string[] args)
         {
-            //Creating an instance of a class for Voice Greeting to allow the voice greeting to play
-            new VoiceGreeting() { };
-
-            //Creating an instance of a class for Display Logo to display the ASCII logo design
-            new DisplayLogo() { };
-
-            //Creating an instance of a class for ChatBot Interaction that handles the interactions between the user and the chatbot, this is where keyword recognition will occur 
-            new ChatBotInteraction() { };
-
-            //************************* PART 2 *********************
-            //1. 
-
-            //2. Creating a class that will have three methods that will:
-            /*
-             * 1. Check and create file
-             * 2. Get what is stored in the text file 
-             * 3. Add or write into the text file
-             */
-
-            //Instatiating the checkTextFile 
-            CheckTextFile checkExist = new CheckTextFile();
-
-            //Using the object called checkExist, I will use this object to get the methods in the checkTextFile class
-
-            //First class to call:
-            checkExist.checkFile();
-
-            //Second class to call:
-            List<string> memory = checkExist.returnMemory();
-
-            //Using a foreach to list all memory values
-            foreach (string check in memory)
+            try
             {
-                Console.WriteLine(check);
-            }//end of foreach loop
+                //Creating an instance of a class for Voice Greeting to allow the voice greeting to play
+                new VoiceGreeting() { };
 
-            //Prompting the user and saving what is entered by the user in the MemoryRecallFile
-
-
-
-            //Third class to call: 
+                //Creating an instance of a class for Display Logo to display the ASCII logo design
+                new DisplayLogo() { };
 
 
+                //************************* PART 2 *********************
+
+                //Instatiating the checkTextFile 
+                CheckTextFile checkExist = new CheckTextFile();
+
+                //Using the object called checkExist, I will use this object to get the methods in the checkTextFile class
+
+                //First class to call: 
+                checkExist.checkFile();
+
+                //Second class to call: Get what is stored in the text file
+                List<string> memory = checkExist.returnMemory();
+
+                //Display existing memory if any
+                if (memory.Count > 0)
+                {
+                    Console.WriteLine("Previous conversation memory found:");
+                    //Using a foreach to list all memory values
+                    foreach (string check in memory)
+                    {
+                        Console.WriteLine(check);
+                    }//end of foreach loop
+                    Console.WriteLine(); // Add spacing
+                }//End of if statement
+
+                //Creating an instance of a class for ChatBot Interaction that handles the interactions between the user and the chatbot, this is where keyword recognition will occur 
+                new ChatBotInteraction() { };
 
 
+                // Third method to call: Save any new memory(this happens automatically within ChatBotInteraction)
+                Console.WriteLine("Thank you for using the Cybersecurity Awareness Chatbot!");
+                Console.WriteLine("Stay safe online!");
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
-    }//end of class 
+        }//end of class 
 }
